4ab6089bd7fe1e18bc475119ec03fba2e3f871a9
kakitomo235 <59069977+kakitomo235@users.noreply.github.com>
2021-01-10 20:14:11 +0900

    rails基礎課題resorces

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
     def create
         User.create(user_params)
-        redirect_to action: :index
+    
     end
     def edit
         @user = User.find(params[:id])
@@ -19,13 +19,12 @@ class UsersController < ApplicationController
     def update
         user = User.find(params[:id])
         user.update(user_params)
-        redirect_to action: :index
     end
 
     def destroy
         user = User.find(params[:id])
         user.destroy
-        redirect_to action: :index
+       
     end
 
 
